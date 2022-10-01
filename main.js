9ad316af474fc3ef7078886403288d44a7a66e4f
unknown <divyanshipatel2008apr13@gmail.com>
2022-09-30 23:33:33 -0700

    new animation file

--- main.js
+++ main.js
@@ -3,20 +3,20 @@ const camera = new THREE.PerspectiveCamera(75 , window.innerWidth/window.innerHe
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth ,window.innerHeight)
 document.body.appendChild(renderer.domElement)
-const geo = new THREE.BoxGeometry( 3,3 , 3)
-const texture = new THREE.TextureLoader().load("ttt.png")
+const geo = new THREE.BoxGeometry( 2.5,2.5 ,2.5)
+const texture = new THREE.TextureLoader().load("tttt.png")
 const material = new THREE.MeshBasicMaterial({map:texture})
 const cube = new THREE.Mesh(geo,material)
 scene.add(cube)
 camera.position.z = 5;
 
-scene.background = new THREE.Color(0x2f3536);
+scene.background = new THREE.Color(0x232524);
 
 
 function animate(){
     requestAnimationFrame(animate)
     renderer.render(scene , camera)
-    cube.rotation.x +=0.01;
-    cube.rotation.y +=0.01;
+    cube.rotation.x +=0.07;
+    cube.rotation.y -=0.05;
 }
 animate()
